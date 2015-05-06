a5b3f379dd8bc5dcd34eb2aad5ae4863807bcc35
Karen <aguirretellez96@gmail.com>
2015-05-05 23:33:57 -0600

    modificacion
    Signed-off-by: Karen <aguirretellez96@gmail.com>

--- config/db.php
+++ config/db.php
@@ -4,6 +4,6 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=seminariointerdiocesano',
     'username' => 'root',
-    'password' => '1234',
+    'password' => '123',
     'charset' => 'utf8',
 ];
